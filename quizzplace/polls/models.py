8c6678f863af5737c615c9195ac14f6d84b8e7b2
Fess <maxzavalov@gmail.com>
2023-10-24 22:56:15 +0400

    add_model_poll

--- quizzplace/polls/models.py
+++ quizzplace/polls/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Poll(models.Model):
+    """Модель опроса."""
+
+
+    name = models.CharField(max_length=100, null=False, blank=False, verbose_name="Название опроса")
+    dateFrom = models.DateTimeField(null=False, blank=False, editable=False, verbose_name="Дата начала опроса")
+    dateTo = models.DateTimeField(null=False, blank=False, editable=False, verbose_name="Дата окончания опроса")
+    isActive = models.BooleanField(default=True, verbose_name="Статус опроса")
+
+    class Meta:
+        verbose_name = "Опрос"
+        verbose_name_plural = "Опросы"
